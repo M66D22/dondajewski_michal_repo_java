15f6859e43ab501ed7ab8c099b7e6419215f19ef
M66D22 <58565791+M66D22@users.noreply.github.com>
2021-03-01 09:29:50 +0100

    Tab2d Commit 2

--- TabDwuWymiarowa.java
+++ TabDwuWymiarowa.java
@@ -1,8 +1,20 @@
 public class TabDwuWymiarowa {
     public static void main(String[]args){
-        int[] tab = new int[5];
-        tab[0] = 40;
-        //tab[1] = "Jeden";
+		tab2dFirst();
 
     }
+	
+	private static void tab2dFirst(){
+		int[][] tab2d =  new int[5][2];
+		
+		tab2d[0][1] = 50;
+		tab2d[2][1] = -200;
+		tab2d[4][0] = 444;
+		//System.out.println(tab2d.length*tab2d[0].length);
+		for (int i=0; i<tab2d.length; i++){
+			for (int j=0; j<tab2d[i].length; j++){
+				System.out.println(tab2d[i][j]);
+			}
+		}
+	}
 }
