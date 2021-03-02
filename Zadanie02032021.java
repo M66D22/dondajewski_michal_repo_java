88f3e03524ebb9e08adae62b4464c99165cae14a
M66D22 <58565791+M66D22@users.noreply.github.com>
2021-03-02 09:58:36 +0100

    Skończone Zadanie

---
+++ Zadanie02032021.java
@@ -0,0 +1,19 @@
+public class Zadanie02032021{
+	public static void main(String[] args){
+		tablica2x2();
+	}
+	
+	public static void tablica2x2(){
+		int[][] tab2d = new int[5][2];
+		tab2d[0][1] = 1;
+		tab2d[1][1] = 2;
+		tab2d[2][0] = 3;
+		tab2d[2][1] = 4;
+		for (int i=0; i<tab2d.length; i++){
+			for (int j=0; j<tab2d[i].length; j++){
+				System.out.println(tab2d[i][j]);
+			}
+		}
+		
+	}
+}
