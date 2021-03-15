a72def48aa06b076ce10313d278795e156e80803
M66D22 <58565791+M66D22@users.noreply.github.com>
2021-03-15 09:00:51 +0100

    Add files via upload

---
+++ ProgramowanieObiektowe/PierwszyProjekt.java
@@ -0,0 +1,11 @@
+public class PierwszyObiekt{
+	public static void main(String[] args){
+		
+	}
+}
+
+class ObiektPudelko{
+	public int wysokosc = 10;
+	public int szerokosc = 20;
+	public int glebokosc = 30;
+}
