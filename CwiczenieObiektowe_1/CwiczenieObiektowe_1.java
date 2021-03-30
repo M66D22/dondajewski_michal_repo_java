4ab0ceb24bfb335bd6c56092c284cb97255f008b
M66D22 <58565791+M66D22@users.noreply.github.com>
2021-03-30 09:10:08 +0200

    Skonczone zadanie 2 1.9.1 Cwiczenia

--- CwiczenieObiektowe_1/CwiczenieObiektowe_1.java
+++ CwiczenieObiektowe_1/CwiczenieObiektowe_1.java
@@ -1,8 +1,10 @@
 class CwiczenieObiektowe_1{
 	public static void main(String[] args){
 		MojeDane md = new MojeDane();
+		Logowanie lo = new Logowanie();
 		System.out.println("Imie: "+md.imie);
 		System.out.println("Nazwisko: "+md.nazwisko);
 		System.out.println("Klasa: "+md.klasa);
+		System.out.println(lo.toString());
 	}
 }
