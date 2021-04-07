ba849e8399030d537180d443256cd36211466b4e
M66D22 <58565791+M66D22@users.noreply.github.com>
2021-04-07 10:29:24 +0200

    Sprawdzian

---
+++ sprawdzian_dondajewski_michal/Sprawdzian.java
@@ -0,0 +1,5 @@
+class Sprawdzian{
+	public String data = "07.04.2021";
+	public int ocena = 5;
+	public String przedmiot = "Programowanie obiektowe";
+}
---
+++ sprawdzian_dondajewski_michal/Tablica.java
@@ -0,0 +1,30 @@
+import java.util.Random;
+
+class Tablica{
+	public int tab1[];
+	public int tab2[];
+	
+	public Tablica(int dlugoscTablicy){
+		tab1 = new int[dlugoscTablicy];
+		tab2 = new int[dlugoscTablicy];
+		
+		wypelnianieTablic();
+	}
+	
+	private void wypelnianieTablic(){
+		Random r = new Random();
+		for (int i = 0; i < tab1.length; i++){
+			tab1[i] = r.nextInt(10) - 10;
+		}
+		for (int i = 0; i < tab2.length; i++){
+			tab2[i] = r.nextInt(10) - 10;
+		}
+	}
+	
+	public int[] pobierzTab1(){
+		return tab1;
+	}
+	public int[] pobierzTab2(){
+		return tab2;
+	}
+}
