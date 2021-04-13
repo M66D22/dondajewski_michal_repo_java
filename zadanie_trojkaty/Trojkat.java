be44bbb73ebf63e19fa7f5d86f6639bb2045a659
M66D22 <58565791+M66D22@users.noreply.github.com>
2021-04-13 09:28:58 +0200

    Stworzenie szkieletu

---
+++ zadanie_trojkaty/Trojkat.java
@@ -0,0 +1,17 @@
+class Trojkat{
+	/*
+	Było kilka kontruktorów bo mamy różne rodzaje trójkątów
+	w zależności od rodzaju trójąta będą w kontruktorze odpowiednie
+	obliczenia POLA
+	zobacz tablice matematyczne strona 10 i 11
+	
+	Rodzaje trójkątów: 
+	równoboczny, równoramienny, różnoboczny, prostokątny, ostrokątny, rozwartokątny
+	*/
+	
+	private int a = 0;
+	private int b = 0;
+	private int c = 0;
+	private h = 0;
+	
+}
